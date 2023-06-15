847531674db3f35734cf47af9cc04de9e074930e
Feng <129321746+huangzhitian@users.noreply.github.com>
2023-06-15 20:46:41 +0800

    Update server.py

--- src/pandora_cloud/server.py
+++ src/pandora_cloud/server.py
@@ -23,7 +23,7 @@ class ChatBot:
         self.login_local = getenv('LOGIN_LOCAL',True)
         self.log_level = logging.DEBUG if debug else logging.WARN
         self.api_prefix = getenv('CHATGPT_API_PREFIX',
-                                 'https://gpt.iqach.top')
+                                 'https://chat-shared1.zhile.io')
 
     @staticmethod
     def after_request(resp):
